38ba692fa90abb71d37801d8b543256c921f072c
Panat Kaewlangka <panut4207315@gmail.com>
2023-12-28 22:33:18 +0700

    completelab12-4

--- lab12_3.cpp
+++ lab12_3.cpp
@@ -1,4 +1,46 @@
 #include<iostream>
+using namespace std;
+
+int fibonacci(int);
+
+int main(){
+    int x;
+    cin >> x;
+    cout << fibonacci(x);
+    return 0; 
+}
+
+int fibonacci(int x){
+    if(x==1 || x==0){
+        return x;
+    }else{
+        return fibonacci(x-1) + fibonacci(x-2);
+    }
+}
+Noprada
+Noprada Krit
+#include<iostream>
+using namespace std;
+
+int gcd(int,int);
+
+int main(){
+    int x, y;
+    cin >> x >> y;
+    cout << gcd(x,y);
+    return 0; 
+}
+
+int gcd(int x,int y){
+    if(x%y==0){
+        return y;
+    }else{
+        return gcd(y, x%y);
+    }
+}
+Noprada
+Noprada Krit
+#include<iostream>
 
 using namespace std;
 
@@ -14,4 +56,11 @@ int main(){
 	cout << "x = " << x << ", y = " << y << "\n";
 	
 	return 0;
+}
+
+void mySwap(int &x,int &y){
+	int a;
+	a=x;
+	x=y;
+	y=a;
 }
