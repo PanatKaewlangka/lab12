0e875198ed46a27e4d69c1c94fc9b0d9fffe2ffe
Panat Kaewlangka <panut4207315@gmail.com>
2023-12-28 22:37:04 +0700

    lab12complete

--- lab12_2.cpp
+++ lab12_2.cpp
@@ -17,29 +17,3 @@ int gcd(int x,int y){
         return gcd(y, x%y);
     }
 }
-Noprada
-Noprada Krit
-#include<iostream>
-
-using namespace std;
-
-void mySwap(int &,int &); 
-
-int main(){
-	int x,y;
-	cin >> x >> y;
-	cout << "Before swapping:\n";
-	cout << "x = " << x << ", y = " << y << "\n";
-	mySwap (x,y);
-	cout << "After swapping:\n";
-	cout << "x = " << x << ", y = " << y << "\n";
-	
-	return 0;
-}
-
-void mySwap(int &x,int &y){
-	int a;
-	a=x;
-	x=y;
-	y=a;
-}
--- lab12_3.cpp
+++ lab12_3.cpp
@@ -1,46 +1,4 @@
 #include<iostream>
-using namespace std;
-
-int fibonacci(int);
-
-int main(){
-    int x;
-    cin >> x;
-    cout << fibonacci(x);
-    return 0; 
-}
-
-int fibonacci(int x){
-    if(x==1 || x==0){
-        return x;
-    }else{
-        return fibonacci(x-1) + fibonacci(x-2);
-    }
-}
-Noprada
-Noprada Krit
-#include<iostream>
-using namespace std;
-
-int gcd(int,int);
-
-int main(){
-    int x, y;
-    cin >> x >> y;
-    cout << gcd(x,y);
-    return 0; 
-}
-
-int gcd(int x,int y){
-    if(x%y==0){
-        return y;
-    }else{
-        return gcd(y, x%y);
-    }
-}
-Noprada
-Noprada Krit
-#include<iostream>
 
 using namespace std;
 
@@ -59,8 +17,7 @@ int main(){
 }
 
 void mySwap(int &x,int &y){
-	int a;
-	a=x;
-	x=y;
-	y=a;
+	int temp = x;
+	x = y;
+	y = temp;
 }
